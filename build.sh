0c7c6fb9cd601c100ab46e797a0a87ac751b98bb
Eric Gratorp <eric.gratorp@gmail.com>
2023-03-27 21:25:49 +0200

    Use standard aarch64 toolchain
    Remove proprietary toolchain prefix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #aarch64-xr-linux-gcc -g -nostdlib --no-pie -c start.S
-aarch64-xr-linux-gcc -march=armv8-a -mcpu=cortex-a53 -g -nostdlib --no-pie -c start.S test.c
-aarch64-xr-linux-ld -g -T start.ld -o start.elf start.o test.o
-aarch64-xr-linux-objcopy -O binary start.elf start.bin
+aarch64-linux-gnu-gcc -march=armv8-a -mcpu=cortex-a53 -g -nostdlib --no-pie -c start.S test.c
+aarch64-linux-gnu-ld -g -T start.ld -o start.elf start.o test.o
+aarch64-linux-gnu-objcopy -O binary start.elf start.bin
